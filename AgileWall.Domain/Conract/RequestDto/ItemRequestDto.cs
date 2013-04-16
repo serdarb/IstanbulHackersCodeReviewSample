d5e63f09bee14a599253179c2d0474aa79bfa969
Serdar Büyüktemiz <hserdarb@gmail.com>
2013-04-16 17:33:47 +0300

    default trims at dto level

--- AgileWall.Domain/Conract/RequestDto/ItemRequestDto.cs
+++ AgileWall.Domain/Conract/RequestDto/ItemRequestDto.cs
@@ -2,7 +2,33 @@
 {
     public class ItemRequestDto
     {
-        public string ItemId { get; set; }
-        public string Text { get; set; }
+        private string _itemId;
+        private string _text;
+
+        public string ItemId
+        {
+            get { return _itemId; }
+            set
+            {
+                if (value != null)
+                {
+                    _itemId = value.Trim();
+                }
+                _itemId = value;
+            }
+        }
+
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                if (value != null)
+                {
+                    _text = value.Trim();
+                }
+                _text = value;
+            }
+        }
     }
 }
--- AgileWall.Domain/Conract/RequestDto/NewOrganizationRequestDto.cs
+++ AgileWall.Domain/Conract/RequestDto/NewOrganizationRequestDto.cs
@@ -2,11 +2,89 @@ namespace AgileWall.Domain.Conract.RequestDto
 {
     public class NewOrganizationRequestDto
     {
-        public string OrganizationName { get; set; }
-        public string OrganizationUrlName { get; set; }
-        public string UserFirstName { get; set; }
-        public string UserLastName { get; set; }
-        public string UserEmail { get; set; }
-        public string UserPassword { get; set; }
+        private string _organizationName;
+        private string _organizationUrlName;
+        private string _userFirstName;
+        private string _userLastName;
+        private string _userEmail;
+        private string _userPassword;
+
+        public string OrganizationName
+        {
+            get { return _organizationName; }
+            set
+            {
+                if (value != null)
+                {
+                    _organizationName = value.Trim();
+                }
+                _organizationName = value;
+            }
+        }
+
+        public string OrganizationUrlName
+        {
+            get { return _organizationUrlName; }
+            set
+            {
+                if (value != null)
+                {
+                    _organizationUrlName = value.Trim();
+                }
+                _organizationUrlName = value;
+            }
+        }
+
+        public string UserFirstName
+        {
+            get { return _userFirstName; }
+            set
+            {
+                if (value != null)
+                {
+                    _userFirstName = value.Trim();
+                }
+                _userFirstName = value;
+            }
+        }
+
+        public string UserLastName
+        {
+            get { return _userLastName; }
+            set
+            {
+                if (value != null)
+                {
+                    _userLastName = value.Trim();
+                }
+                _userLastName = value;
+            }
+        }
+
+        public string UserEmail
+        {
+            get { return _userEmail; }
+            set
+            {
+                if (value != null)
+                {
+                    _userEmail = value.Trim();
+                }
+                _userEmail = value;
+            }
+        }
+
+        public string UserPassword
+        {
+            get { return _userPassword; }
+            set
+            {
+                if (value != null)
+                {
+                    _userPassword = value.Trim();
+                }
+                _userPassword = value;
+            }
+        }
     }
 }
